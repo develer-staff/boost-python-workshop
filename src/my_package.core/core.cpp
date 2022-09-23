a88d910df142d4b6df62f87af4f23216eb6a6849
Gianni Valenti <gvalenti@develer.com>
2022-09-23 09:49:10 +0200

    Add my_char_ptr class

--- src/my_package.core/core.cpp
+++ src/my_package.core/core.cpp
@@ -1,8 +1,10 @@
 #include <boost/python.hpp>
 
 #include "my_class.h"
+#include "my_char_ptr.h"
 
 BOOST_PYTHON_MODULE(core)
 {
     my_class::export_class();
+    my_char_ptr::export_class_converter();
 }
