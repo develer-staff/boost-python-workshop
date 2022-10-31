e65a300096a3f5629cb7d4ef06682373518851c2
Gianni Valenti <gvalenti@develer.com>
2022-10-31 09:36:00 +0100

    Add my_char_ptr class

---
+++ src/my_package.core/my_char_ptr.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stddef.h>
+#include <utility>
+
+class my_char_ptr : std::pair<const char*, size_t>
+{
+public:
+    my_char_ptr(const char* ptr, size_t size) : std::pair<const char*, size_t>(ptr, size) { }
+
+    const char* ptr() const
+    {
+        return first;
+    }
+
+    size_t size() const
+    {
+        return second;
+    }
+
+    static void export_class_converter();
+};
