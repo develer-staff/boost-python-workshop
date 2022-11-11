7e73380dca7479dfaa71ef7224a1fba5079eede7
Gianni Valenti <gvalenti@develer.com>
2022-11-11 11:12:09 +0100

    Add name C++ property

--- src/my_package.core/my_class.cpp
+++ src/my_package.core/my_class.cpp
@@ -9,6 +9,17 @@ void my_class::say_hello() const
     std::cout << "Hello there! I'm a C++ method." << std::endl;
 }
 
+const char* my_class::get_name() const
+{
+    return m_name.c_str();
+}
+
+void my_class::set_name(const char* name)
+{
+    std::cout << "my_class::set_name('" << name << "')" << std::endl;
+    m_name = name;
+}
+
 // Reference:
 // https://www.boost.org/doc/libs/1_74_0/libs/python/doc/html/tutorial/tutorial/exposing.html
 void my_class::export_class()
--- src/my_package.core/my_class.h
+++ src/my_package.core/my_class.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <string>
+
 class my_class
 {
 public:
     void say_hello() const;
 
+    const char* get_name() const;
+    void set_name(const char* name);
+
     static void export_class();
+
+private:
+    std::string m_name;
 };
